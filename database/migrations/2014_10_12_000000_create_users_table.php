0296dfcd4ecfd0696bb70d40b23e5858fbe1be74
tyCodes <oluwatofunmi.yusuf@gmail.com>
2017-10-03 14:36:58 +0100

    added new files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,28 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('username')->unique();
+            $table->integer('phone number');
+            $table->string('home address');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('admin', ['admin','non-admin']);
+            $table->integer('status',1);
+            $table->string('next of kin');
+            $table->string('phonenumber of kin');
             $table->timestamps();
+
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('users');
