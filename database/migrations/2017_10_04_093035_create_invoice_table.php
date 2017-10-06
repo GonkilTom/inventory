f20fd1c02168a1aad2123adfd11549b6611e2159
tyCodes <oluwatofunmi.yusuf@gmail.com>
2017-10-06 14:26:53 +0100

    changed to timestamps in all tables

---
+++ database/migrations/2017_10_04_093035_create_invoice_table.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateInvoiceTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('invoice', function(Blueprint $table){
+          $table->increments('id');
+          $table->float('total');
+          $table->enum('payment_status',['PAID','PENDING']);
+          $table->timestamps();
+        });
+    }
+    public function down()
+    {
+        Schema::dropIfExists('invoice');
+    }
+}
